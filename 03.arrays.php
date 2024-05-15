95c1f125cda7f9537108d305a314a6362b7dd012
citrino88 <almeida.citrino@gmail.com>
2024-05-15 16:42:28 -0300

    arrays saidas e depuraçao de arrays

--- 03.arrays.php
+++ 03.arrays.php
@@ -13,12 +13,62 @@
     <div class="container">
         <h1>Arrays</h1>
         <hr class="border border-primary border-2 opacity-50">
+        <h2>Arrays numéricos/indexados.</h2>
     <?php
-        
-    ?>
-    <h2>Resolução 01: Forma simplificada</h2>
+        // Declarando arrays como variáveis e constantes
+    $bandas = ["Rush", "Slayer", "Nightwish"];
+    $cursos = array("HTML", "PHP", "Design", "JS");
     
-    <h5>Observação: Boa prática do PHP é declarar constantes em MAIÚSCULO</h5>
+    define("UNIDADES_SESC", ["Belenzinho", "Bertioga"]);
+    const UNIDADES_SENAC = ["Penha", "São Miguel", "Itaquera"];
+    ?>
+
+        <h3>Acesso a saída de dados</h3>
+        <p>Curto a banda <?=$bandas[1]?>.</p>   
+        <p>Já estudei <?=$cursos[3]?></p>
+        <p>Gostaria de conhecer o Sesc <?=UNIDADES_SESC[1]?></p>
+        <p>Estamos no Senac <?=UNIDADES_SENAC[0]?></p>
+        <h6>Observação: Boa prática do PHP é declarar constantes em MAIÚSCULO</h6>
+    <hr class="border border-primary border-2 opacity-50">
+        <h2>Arrays associoativos</h2>
+        <?php
+
+        $filme = [
+        // Chave associativa
+            "titulo" => "Deadpool 2",
+            "ano" => 2018,
+            "genero" => "Ação",
+            "personagens" => ["Wade Wilson", "Fanático"]
+        ];
+
+        $livro = array(
+            "titulo" => "Senhor dos Anéis",
+            "autor" => "Tolkien"
+        );
+        ?>
+        <h3>Acesso e saída de dados</h3>
+        <p>O filme
+            <?=$filme["titulo"]?> foi lançado em <?=$filme['ano']?>
+        </p>
+        <p> <?= $livro['titulo']?> é um ótimo livro. </p>
+
+        <hr>
+
+        <h2> Comandos úteis para análise/ depuração de arrays</h2>
+        <h3><code>print_r()</code> e <code>var_dump()</code></h3>
+
+        <?=print_r($bandas)?>
+
+        <hr>
+
+        <?=var_dump($bandas)?>
+
+        <pre><?=print_r($bandas)?></pre>
+
+        <hr>
+
+        <pre><?=var_dump($bandas)?></pre>
+        <h6><code>pre</code> traz uma pre-formatação, bom para ver códigos</h6>
     </div>
 </body>
 </html>
