02f0df5562ee617ba87ef74b746f77c3b9ddc120
citrino88 <almeida.citrino@gmail.com>
2024-05-15 16:02:09 -0300

    Create 03.arrays.php

---
+++ 03.arrays.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Arrays</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
+    <style>
+      
+    </style>
+</head>
+<body>
+    <div class="container">
+        <h1>Arrays</h1>
+        <hr class="border border-primary border-2 opacity-50">
+    <?php
+        
+    ?>
+    <h2>Resolução 01: Forma simplificada</h2>
+    
+    <h5>Observação: Boa prática do PHP é declarar constantes em MAIÚSCULO</h5>
+    </div>
+</body>
+</html>
