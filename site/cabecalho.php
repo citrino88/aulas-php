5b87862d2946afeec7fe92adfb883af588712554
citrino88 <almeida.citrino@gmail.com>
2024-05-21 17:24:39 -0300

    Update cabecalho.php

--- site/cabecalho.php
+++ site/cabecalho.php
@@ -9,7 +9,7 @@
     ?>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title><?=$title?></title>
+    <title><?=$title?> - Site</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
     <style>
         body{
