147516c77d111363c215c8964890552bc4eff7a9
citrino88 <almeida.citrino@gmail.com>
2024-05-22 15:24:17 -0300

    isset com $_POST

--- 08-formulario-com-processamento.php
+++ 08-formulario-com-processamento.php
@@ -10,7 +10,24 @@
 <body>
     <h1>Formulário e processamento juntos</h1>
     <hr>
-    <form action="" method="post"> <label for="nome">Nome:</label> <input type="text" name="nome" id="nome"> <button type="submit" name="enviar">Enviar</button> </form>
+
+<?php 
+/* Detectando o envio/acionamento do formulario , isset (se está definido ou se existe, no caso abaixo, se o botão enviar foi acionado.*/
+if ( isset($_POST['enviar']) ) {
+    $nome = $_POST['nome'];
+?>
+<h2>Bem-vindo(a) <?= $nome?>!</h2>
+<hr>
+<?php    
+} else{
+    /* O formulário abaixo será exibido enquanto o botão enviar não for acionado. Quando for acionado, o formulário será removido da página. */
+?>    
+
+    <!-- Mantenha o action vazio!!!, pois é processamento na própria página -->
+    <form action="" method="post"> <label for="nome">Nome:</label> <input  required type="text" name="nome" id="nome"> <button type="submit" name="enviar">Enviar</button> </form>
+
+<?php } ?>    
+
 </body>
 
 </html>
