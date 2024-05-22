68ba6f52bb6f86b25babb092be65b04864c2042f
citrino88 <almeida.citrino@gmail.com>
2024-05-22 15:09:14 -0300

    processamento na página navegador

---
+++ 08-formulario-com-processamento.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Formulário com PHP</title>
+</head>
+
+<body>
+    <h1>Formulário e processamento juntos</h1>
+    <hr>
+    <form action="" method="post"> <label for="nome">Nome:</label> <input type="text" name="nome" id="nome"> <button type="submit" name="enviar">Enviar</button> </form>
+</body>
+
+</html>
